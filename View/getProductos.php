ce10751ccc12e8ba4ad9084ec528de920e88fe52
Jorge Montes <56244712+jooge1998@users.noreply.github.com>
2023-03-05 16:21:43 -0500

    eliminacion archivo getproductos.php

--- View/getProductos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once './../Controllers/controllersProductos.php';
-$controllers = new ControllerProductos();
-
-
-$data = $controllers->getId($_POST['id']);
-
-echo json_encode($data);
